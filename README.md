bb688ec63c6940661ada9ca54f28355de380fd54
Fredrik Skatland <fredrikskatland@gmail.com>
2023-11-19 11:18:11 +0100

    Added files

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# gpt-plugins
-repo for gpt-pugins backends
+# GPT Content and Product Retrieval API
+
+## Overview
+This API retrieves relevant documents for a given query, utilizing vector stores and advanced embeddings. It's designed to serve different domains including nursing and baby care (Ammehjelpen), brewing products (Brewshop), and running shoes (Löplabbet).
+
+## Features
+- **Multiple Domains**: Retrieve information from various specialized domains.
+- **Advanced Search Capabilities**: Leverages OpenAI embeddings for efficient and relevant document retrieval.
+- **Easy Integration**: Designed for straightforward integration with existing systems.
+
+## Getting Started
+
+### Prerequisites
+- Python 3.x
+- Quart
+- Other dependencies listed in `requirements.txt`
+
+### Installation
+1. Clone the repository:
+   ```bash
+   git clone [repository-url]
+   ```
+2. Install the required packages:
+   ```bash
+   pip install -r requirements.txt
+   ```
+3. Start the server:
+   ```
+   python main.py
+
+   ```
+
+## Running the API
+To start the server, run `python main.py`. The API will be available at `http://localhost:5003`.
+
+## API Endpoints
+
+### 1. Ammehjelpen Retrieve
+- **Endpoint**: `/ammehjelpen_retrieve`
+- **Method**: POST
+- **Description**: Retrieves documents related to nursing and baby care from Ammehjelpen.
+
+### 2. Brewshop Retrieve
+- **Endpoint**: `/brewshop_retrieve`
+- **Method**: POST
+- **Description**: Retrieves documents related to brewing products from Brewshop.
+
+### 3. Löplabbet Retrieve
+- **Endpoint**: `/loplabbet_retrieve`
+- **Method**: POST
+- **Description**: Retrieves documents related to running shoes from Löplabbet.
+
+### Additional Endpoints
+- **Plugin Manifest**: `/.well-known/ai-plugin.json`
+- **OpenAPI Specification**: `/openapi.yaml`
+
+## Usage
+To use the API, send a POST request to the desired endpoint with a JSON payload containing the query. For example:
+
+```json
+{
+  "query": "best running shoes for marathons"
+}
---
+++ main.py
@@ -0,0 +1,132 @@
+import json
+import pickle
+import quart
+import quart_cors
+from quart import request
+import os
+
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+embeddings = OpenAIEmbeddings()
+
+# Import documents.pickle
+#with open("documents.pickle", "rb") as f:
+#    documents = pickle.load(f)
+
+# Create a Chroma database
+#db = Chroma.from_documents(documents, embeddings)
+ammehjelpen_db = Chroma(persist_directory="./vectorstores/ammehjelpen_chroma_db", embedding_function=embeddings)
+ammehjelpen_retriever = ammehjelpen_db.as_retriever()
+
+brewshop_db = Chroma(persist_directory="./vectorstores/brewshop_chroma_db", embedding_function=embeddings)
+brewshop_retriever = brewshop_db.as_retriever()
+
+loplabbet_db = Chroma(persist_directory="./vectorstores/loplabbet_chroma_db", embedding_function=embeddings)
+loplabbet_retriever = loplabbet_db.as_retriever()
+
+def convert_to_dict(result):
+    page_content = [doc.page_content for doc in result]
+    metadatas = [doc.metadata for doc in result]
+
+    output = []
+    for i in range(len(page_content)):
+        output.append({'page_content': page_content[i], 'metadata': metadatas[i]})
+
+    return output
+
+app = quart_cors.cors(quart.Quart(__name__))
+
+@app.post("/ammehjelpen_retrieve")
+async def retrieve():
+    request = await quart.request.get_json(force=True)
+    query = request["query"]
+    result = ammehjelpen_retriever.get_relevant_documents(query)
+    output = convert_to_dict(result)
+    # Convert Documents to 
+    return quart.Response(response=json.dumps(output), status=200)
+
+@app.post("/brewshop_retrieve")
+async def retrieve():
+    request = await quart.request.get_json(force=True)
+    query = request["query"]
+    result = brewshop_retriever.get_relevant_documents(query)
+    output = convert_to_dict(result)
+    # Convert Documents to 
+    return quart.Response(response=json.dumps(output), status=200)
+
+@app.post("/loplabbet_retrieve")
+async def retrieve():
+    request = await quart.request.get_json(force=True)
+    query = request["query"]
+    result = loplabbet_retriever.get_relevant_documents(query)
+    output = convert_to_dict(result)
+    # Convert Documents to 
+    return quart.Response(response=json.dumps(output), status=200)
+
+@app.get("/.well-known/ai-plugin.json")
+async def plugin_manifest():
+    host = request.headers['Host']
+    with open("./.well-known/ai-plugin.json") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/json")
+
+@app.get("/openapi.yaml")
+async def openapi_spec():
+    host = request.headers['Host']
+    with open("openapi.yaml") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/yaml")
+
+@app.get("/privacy")
+async def privacy_policy():
+    # Method 1: Serve a static file (e.g., HTML or TXT)
+    #return await quart.send_from_directory(directory='path_to_directory', filename='privacy_policy.html')
+
+    # Method 2: Return a string
+    privacy_text = """
+    Privacy Policy for the GPT API calls
+
+Last Updated: 2023-11-18
+
+1. Introduction
+
+Welcome to the GPT API. We are committed to respecting your privacy and protecting your personal information. This Privacy Policy explains the types of information we collect through our API and how we use this information.
+
+2. Information We Collect
+
+The GPT API is designed to collect only the information that is necessary to provide our service. The only data we collect is:
+
+Input Data: This includes the query argument that you provide when you make an API call. We use this data solely for the purpose of processing your request and providing you with the relevant response.
+3. Use of Collected Information
+
+The information we collect is used in the following ways:
+
+To process and respond to the queries received via the API.
+To improve the functionality and performance of our API.
+We do not use the information collected for any other purpose, nor do we share it with any third parties.
+
+4. Data Retention
+
+We retain the collected data only as long as necessary to provide the requested service. Once the purpose of data collection is fulfilled, we ensure the data is securely deleted from our systems.
+
+5. Security
+
+We take the security of your data seriously and implement appropriate technical and organizational measures to protect it against unauthorized or unlawful access, alteration, disclosure, or destruction.
+
+6. Changes to This Privacy Policy
+
+We may update this Privacy Policy from time to time. We encourage you to review this policy periodically to stay informed about how we are protecting the information we collect.
+
+7. Contact Us
+
+If you have any questions or concerns about this Privacy Policy or our data practices, please contact us at [Your Contact Information].
+"""
+    return quart.Response(response=privacy_text, status=200, mimetype='text/plain')
+
+
+def main():
+    app.run(debug=True, host="0.0.0.0", port=5003)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5003))
+    app.run(debug=True, host="0.0.0.0", port=port)
