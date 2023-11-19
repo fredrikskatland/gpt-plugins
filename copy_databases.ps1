6768e3833135c831ca434488b928ed7df12b0f3f
Fredrik Skatland <fredrikskatland@gmail.com>
2023-11-19 11:27:42 +0100

    Added copy databases ps1

---
+++ copy_databases.ps1
@@ -0,0 +1,3 @@
+Copy-Item -Path "..\brewshop-gpt\brewshopplugin\brewshop_chroma_db\" -Destination ".\vectorstores\" -Recurse
+Copy-Item -Path "..\ammehjelpen\ammehjelpen-chatbot\ammehjelpen_chroma_db\" -Destination ".\vectorstores\" -Recurse
+Copy-Item -Path "..\loplabbet-plugin\loplabbet_chroma_db\" -Destination ".\vectorstores\" -Recurse       
